e7192957d60044c9a8e26a0a26c4c4ffdf276329
npeltie1 <nik.peltier@uhg.com>
2020-11-19 12:08:01 -0600

    Local file import with LoadLocal project description

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/project/SingleDocumentProjectSourcesExtractor.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/project/SingleDocumentProjectSourcesExtractor.java
@@ -45,7 +45,7 @@ public class SingleDocumentProjectSourcesExtractor implements RawProjectSourcesE
 
         @Override
         public OWLOntologyIRIMapper getOntologyIRIMapper() {
-            return new AutoIRIMapper(new File("/srv/webprotege/ontologies/ontologies"), true);
+            return new AutoIRIMapper(new File("/srv/webprotege/ontologies/ontologies"), false);
         }
 
         @Override
